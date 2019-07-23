eabcc66f0d05033c22114430efd3f3543c48ef55
Kevin Gray <kevingray.boston@gmail.com>
2019-07-23 11:48:53 -0400

    init commit

---
+++ thirdrock.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer(function (req, res) {  //req = request, res = response.
+  fs.readFile('demofile.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8000);
+
+
+// require('http').createServer(function (req, res) {
